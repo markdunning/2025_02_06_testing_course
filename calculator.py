647c88d1d74aa6ec6f812ef20285f67ab4b14a69
Mark Dunning <markdunning@Marks-MacBook-Pro.local>
2025-02-06 16:03:25 +0000

    initial commit

---
+++ calculator.py
@@ -0,0 +1,7 @@
+def add(a, b):
+  return a + b
+
+def divide(numerator, denominator):
+    if denominator == 0:
+        raise ZeroDivisionError("Cannot divide by zero!")
+    return numerator / denominator
---
+++ test_calculator.py
@@ -0,0 +1,20 @@
+# Import the add function so the test can use it
+import pytest
+from calculator import add
+from calculator import divide
+
+def test_add():
+   # Check that it adds two positive integers
+   assert add(1, 2) == 3
+   
+   # Check that it adds zero
+   assert add(5, 0) == 5
+   
+   # Check that it adds two negative integers
+   assert add(-1, -2) == -3
+
+  
+def test_divide():
+  with pytest.raises(ZeroDivisionError) as e:
+        divide(1,0)
+  assert str(e.value) == "Cannot divide by zero!"
